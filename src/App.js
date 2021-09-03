5d5d46294209bf42dffee53fa3217864b319c455
Rohit_Joshi <rohitjoshi632001@gmail.com>
2021-09-03 15:28:28 +0530

    React icons and task comp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import { useState } from "react"
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 
-function App() {
+const App = ()=>{
+  const [tasks, setTasks]=useState([
+    {
+        id:1,
+        text: 'Doctor Appointment',
+        day: 'Sept 7th at 2:30pm',
+        reminder:true,
+    },
+    {
+        id:2,
+        text: 'GDSC Meeting',
+        day: 'Sept 8th at 1:30pm',
+        reminder:true,
+    },
+    {
+        id:3,
+        text: 'Family Dinner',
+        day: 'Sept 9th at 8:30pm',
+        reminder:false,
+    }
+])
+
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
